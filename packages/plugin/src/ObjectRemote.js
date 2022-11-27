965fbdef26a5d12595d3cf443ff3431648dcf124
Alan Schio <alanbschio@gmail.com>
2022-11-27 00:05:34 -0300

    feat(RemoteObject): add validation for name and url required

--- packages/plugin/src/ObjectRemote.js
+++ packages/plugin/src/ObjectRemote.js
@@ -18,8 +18,10 @@ const validateAndSetDefault = (remote) => {
 };
 
 const mountFinalRemoteValue = (remote, defaultAsync) => {
+  if(!remote.name || !remote.url) {
+    throw new Error("ModuleFederationEnhancedPlugin: `name` and `url` are required when define a remote on object format.")
+  }
   if (remote.async || defaultAsync) {
-    // remote = validateAndSetDefault(remote);
     return handleDefaultAsync(remote);
   }
   return `${remote.name}@${remote.url}/${remote.remoteEntry}`;
@@ -45,7 +47,7 @@ const handleRemotes = (remotes, defaultAsync) => {
   const _newRemotes = {};
   Object.keys(remotes || {})?.forEach((remoteName) => {
     const remote = remotes[remoteName];
-    _newRemotes[remoteName] = defaultAsync || remote.true
+    _newRemotes[remoteName] = defaultAsync || remote.async
       ? createDefaultAsync(remote)
       : createNotDefaultAsync(remote);
   });
